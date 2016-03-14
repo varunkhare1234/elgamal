37317bae40a84dd39f5db25c788330e04d73e355
varun <varunkhare1234@gmail.com>
2016-03-14 20:10:47 +0530

    co prime function added

--- keygen.py
+++ keygen.py
@@ -2,6 +2,9 @@
 import random
 import sys
 def main():
+    p=2**3217-1
+    g=random.randint(10**500,10**800)
+    while not g%p
     g=random.randint(10**500,10**800)
     x=random.randint(10**500,10**800)
     f=open("/home/varun/elgamal/sourcefile",'w+')
